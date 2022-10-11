e19fd5b12bc7713f7988b94b69bd81433fe2b87d
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-10-11 13:15:13 +0300

    feat: add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [HeaderComponent],
+})
+export class CoreModule {}
