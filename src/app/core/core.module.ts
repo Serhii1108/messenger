197f8695589e892427d9dbaf56572273213e3be1
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-02-25 18:44:39 +0200

    feat: add page not found component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, PageNotFoundComponent],
   imports: [CommonModule, SharedModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, PageNotFoundComponent],
 })
 export class CoreModule {}
