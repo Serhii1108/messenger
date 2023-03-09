5240087ffa434e0925ef91fbe11839d912bf4cc8
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-03-09 22:09:11 +0200

    feat: add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PageNotFoundComponent],
+  declarations: [HeaderComponent, PageNotFoundComponent, FooterComponent],
   imports: [CommonModule, SharedModule],
-  exports: [HeaderComponent, PageNotFoundComponent],
+  exports: [HeaderComponent, PageNotFoundComponent, FooterComponent],
 })
 export class CoreModule {}
