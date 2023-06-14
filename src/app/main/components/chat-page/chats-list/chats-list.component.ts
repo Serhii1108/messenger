4b98d4e9fcf36c80552845c324ca193e2c81e41c
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-06-14 14:56:13 +0300

    feat: add chats list component

---
+++ src/app/main/components/chat-page/chats-list/chats-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chats-list',
+  templateUrl: './chats-list.component.html',
+  styleUrls: ['./chats-list.component.scss'],
+})
+export class ChatsListComponent {}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ChatPageComponent } from './components/chat-page/chat-page.component';
+import { ChatsListComponent } from './components/chat-page/chats-list/chats-list.component';
 
 @NgModule({
-  declarations: [MainPageComponent, ChatPageComponent],
+  declarations: [MainPageComponent, ChatPageComponent, ChatsListComponent],
   imports: [CommonModule, SharedModule, MainRoutingModule],
   exports: [MainPageComponent],
 })
