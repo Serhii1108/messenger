292a502e90c7a46b6b4e89608c3a9e1e134717da
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-06-20 20:34:46 +0300

    feat: add toggle class active in groups

--- src/app/main/components/chat-page/chats-list/chats-list.component.ts
+++ src/app/main/components/chat-page/chats-list/chats-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-chats-list',
   templateUrl: './chats-list.component.html',
   styleUrls: ['./chats-list.component.scss'],
 })
-export class ChatsListComponent {
+export class ChatsListComponent implements AfterViewInit {
   public searchValue: string = '';
   public groups: [] = [];
   public contacts: [] = [];
+
+  public ngAfterViewInit(): void {
+    const groups: NodeListOf<Element> = document.querySelectorAll('.group');
+
+    groups.forEach((group: Element) => {
+      group.addEventListener('click', () => {
+        document.querySelector('.group.active')?.classList.remove('active');
+        group.classList.add('active');
+      });
+    });
+  }
 }
