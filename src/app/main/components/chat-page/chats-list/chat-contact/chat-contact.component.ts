307cac69b1673f12c82fc35331055142281bd80a
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-06-20 20:41:56 +0300

    feat: add toggle class active in contacts

--- src/app/main/components/chat-page/chats-list/chat-contact/chat-contact.component.ts
+++ src/app/main/components/chat-page/chats-list/chat-contact/chat-contact.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat-contact',
   templateUrl: './chat-contact.component.html',
   styleUrls: ['./chat-contact.component.scss'],
 })
-export class ChatContactComponent {}
+export class ChatContactComponent implements AfterViewInit {
+  public ngAfterViewInit(): void {
+    const contacts: NodeListOf<Element> = document.querySelectorAll('.contact');
+
+    contacts.forEach((contact: Element) => {
+      contact.addEventListener('click', () => {
+        document.querySelector('.contact.active')?.classList.remove('active');
+        contact.classList.add('active');
+      });
+    });
+  }
+}
