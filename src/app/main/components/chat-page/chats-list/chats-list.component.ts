e960b75a1b7d86f9b6a10753990ab83815610441
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-07-01 18:07:34 +0300

    feat: add contact pin

--- src/app/main/components/chat-page/chats-list/chats-list.component.ts
+++ src/app/main/components/chat-page/chats-list/chats-list.component.ts
@@ -11,12 +11,36 @@ export class ChatsListComponent implements AfterViewInit {
   public contacts: [] = [];
 
   public ngAfterViewInit(): void {
+    this.addGroupsEventListeners();
+    this.addContactsEventListeners();
+  }
+
+  private addGroupsEventListeners(): void {
     const groups: NodeListOf<Element> = document.querySelectorAll('.group');
 
     groups.forEach((group: Element) => {
       group.addEventListener('click', () => {
         document.querySelector('.group.active')?.classList.remove('active');
         group.classList.add('active');
+      });
+    });
+  }
+
+  private addContactsEventListeners(): void {
+    const contacts: NodeListOf<Element> = document.querySelectorAll('.contact');
+
+    contacts.forEach((contact: Element) => {
+      contact.addEventListener('click', (e: Event) => {
+        if (e.target == pinBtn) {
+          return;
+        }
+        document.querySelector('.contact.active')?.classList.remove('active');
+        contact.classList.add('active');
+      });
+
+      const pinBtn: Element | null = contact.querySelector('.pin-img');
+      pinBtn?.addEventListener('click', () => {
+        pinBtn.classList.toggle('active');
       });
     });
   }
