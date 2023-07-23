10b6e758bf55141381ed5b459f6b7cbb6fbb88d7
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-07-23 20:49:49 +0300

    feat: add active chat component

---
+++ src/app/main/components/chat-page/active-chat/active-chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-chat',
+  templateUrl: './active-chat.component.html',
+  styleUrls: ['./active-chat.component.scss'],
+})
+export class ActiveChatComponent {
+  public valueToSend: string = '';
+}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -7,6 +7,8 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 import { ChatPageComponent } from './components/chat-page/chat-page.component';
 import { ChatsListComponent } from './components/chat-page/chats-list/chats-list.component';
 import { ChatContactComponent } from './components/chat-page/chats-list/chat-contact/chat-contact.component';
+import { ActiveChatComponent } from './components/chat-page/active-chat/active-chat.component';
+import { MessageComponent } from './components/chat-page/active-chat/message/message.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { ChatContactComponent } from './components/chat-page/chats-list/chat-con
     ChatPageComponent,
     ChatsListComponent,
     ChatContactComponent,
+    ActiveChatComponent,
+    MessageComponent,
   ],
   imports: [CommonModule, SharedModule, MainRoutingModule],
   exports: [MainPageComponent],
